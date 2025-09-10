ca262dce4a8f1292b58ef5b8e02a275c6943bbab
Saood Ahmed <107537712+SaoodAhmed@users.noreply.github.com>
2025-09-10 15:12:43 -0700

    cv-added

--- src/app/components/sidebar.tsx
+++ src/app/components/sidebar.tsx
@@ -7,6 +7,7 @@ import { Linkedin } from "./icons/linkedin";
 import { IoLogoWhatsapp } from "react-icons/io";
 import Image from "next/image";
 import { BsGithub } from "react-icons/bs";
+import { HiOutlineDownload } from "react-icons/hi";
 
 interface PersonalDataType {
   name: string;
@@ -20,7 +21,7 @@ const Sidebar: React.FC<{ data: PersonalDataType }> = ({ data }) => {
   return (
     <div className="bg-black flex flex-col  content-between w-full h-auto sm:h-screen sm:justify-around sm:w-4/12 sm:fixed">
       <div className="flex flex-col p-10 items-center text-white">
-        <Image priority className="rounded-full h-full mb-6" src="/profile.jpg" height={300} width={300} alt="saud_ahmed.profile" aria-label="saud_ahmed.profile"/>
+        <Image priority className="rounded-full h-full mb-6" src="/profile.jpg" height={300} width={300} alt="saud_ahmed.profile" aria-label="saud_ahmed.profile" />
         <h1 className=" text-yellow">
           {name}
         </h1>
@@ -29,11 +30,11 @@ const Sidebar: React.FC<{ data: PersonalDataType }> = ({ data }) => {
         </h2>
         <div className="flex flex-col justify-start">
 
-        <p className="mb-5 leading-normal tracking-wider">{education[0]}</p>
-        <p className="mb-5 leading-normal tracking-wider">{education[1]}</p>
-        <div className="text-center mb-4 mt-4 sm:mt-8">
-        </div>
-          <h2 className=" mx-auto">
+          <p className="mb-5 leading-normal tracking-wider">{education[0]}</p>
+          <p className="mb-5 leading-normal tracking-wider">{education[1]}</p>
+          <div className="text-center mb-4 mt-4 sm:mt-8">
+          </div>
+          <h2 className=" mx-auto mb-2">
             Contact me
           </h2>
           <div className="flex flex-row justify-center gap-2">
@@ -52,7 +53,18 @@ const Sidebar: React.FC<{ data: PersonalDataType }> = ({ data }) => {
             <Link href={contactLinks[3]} aria-label="">
               <IoLogoWhatsapp className="icons-contactme" />
             </Link>
+
           </div>
+          <Link
+            href="/Saud_Ahmed_s_Résumé.pdf"
+            download="Saud_Ahmed_s_Résumé.pdf"
+            aria-label="Download CV"
+            rel="noopener noreferrer"
+            className="inline-flex mx-auto  mt-2 items-center gap-2 px-4 py-2 rounded-2xl bg-indigo-600 hover:bg-indigo-700 text-white text-sm font-medium shadow-md focus:outline-none focus:ring-2 focus:ring-indigo-400 focus:ring-offset-2 transition"
+          >
+            <HiOutlineDownload className={"text-2xl"} />
+            <p >Download CV</p>
+          </Link>
         </div>
       </div>
     </div>
